6a4dba744a1869ea041f5c3daf24efb5e9e1589f
dzulkifli578 <99547157+dzulkifli578@users.noreply.github.com>
2023-08-17 19:03:14 +0700

    Add files via upload

--- Jurus-Pamungkas.cpp
+++ Jurus-Pamungkas.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 
-void print_a (unsigned short batas);
-void print_e (unsigned short batas);
+void print_a (unsigned short masukan);
+void print_e (unsigned short masukan);
 
 int main ()
 {
-    unsigned short batas = 0;
+    unsigned short batas = 0, masukan = 0;
 
     std::cin >> batas;
 
-    std::cout << "k"; print_a (batas);
+    for (unsigned short i = 1; i <= batas; i++)
+    {
+        std::cin >> masukan;
 
-    std::cout << "m"; print_e (batas);
+        std::cout << "k"; print_a (masukan);
 
-    std::cout << "h"; print_a (batas);
+        std::cout << "m"; print_e (masukan);
 
-    std::cout << "m"; print_e (batas);
+        std::cout << "h"; print_a (masukan);
 
-    std::cout << "h"; print_a (batas);
+        std::cout << "m"; print_e (masukan);
+
+        std::cout << "h"; print_a (masukan);
+
+        std::cout << "\n";
+    }
     
     return 0;
 }
 
-void print_a (unsigned short batas)
+void print_a (unsigned short masukan)
 {
-    for (unsigned short i = 1; i <= batas; i++)
+    for (unsigned short i = 1; i <= masukan; i++)
     {
         std::cout << "a";
     }
 }
 
-void print_e (unsigned short batas)
+void print_e (unsigned short masukan)
 {
-    for (unsigned short i = 1; i <= batas; i++)
+    for (unsigned short i = 1; i <= masukan; i++)
     {
         std::cout << "e";
     }

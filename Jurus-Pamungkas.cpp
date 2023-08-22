1ee442335d32ea77ab414c4a4221afe107c2e9cc
dzulkifli578 <99547157+dzulkifli578@users.noreply.github.com>
2023-08-22 17:53:05 +0700

    Implementasi pointer pada Jurus-Pamungkas.cpp

--- Jurus-Pamungkas.cpp
+++ Jurus-Pamungkas.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-void print_a (unsigned short masukan);
-void print_e (unsigned short masukan);
+void print_a (unsigned short *masukan);
+void print_e (unsigned short *masukan);
 
 int main ()
 {
@@ -13,33 +13,28 @@ int main ()
     {
         std::cin >> masukan;
 
-        std::cout << "k"; print_a (masukan);
-
-        std::cout << "m"; print_e (masukan);
-
-        std::cout << "h"; print_a (masukan);
-
-        std::cout << "m"; print_e (masukan);
-
-        std::cout << "h"; print_a (masukan);
-
+        std::cout << "k"; print_a (&masukan);
+        std::cout << "m"; print_e (&masukan);
+        std::cout << "h"; print_a (&masukan);
+        std::cout << "m"; print_e (&masukan);
+        std::cout << "h"; print_a (&masukan);
         std::cout << "\n";
     }
     
     return 0;
 }
 
-void print_a (unsigned short masukan)
+void print_a (unsigned short *masukan)
 {
-    for (unsigned short i = 1; i <= masukan; i++)
+    for (unsigned short i = 1; i <= *masukan; i++)
     {
         std::cout << "a";
     }
 }
 
-void print_e (unsigned short masukan)
+void print_e (unsigned short *masukan)
 {
-    for (unsigned short i = 1; i <= masukan; i++)
+    for (unsigned short i = 1; i <= *masukan; i++)
     {
         std::cout << "e";
     }
